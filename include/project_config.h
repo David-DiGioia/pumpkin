42f281b98278b365e912e4de989bf41475b31827
David DiGioia <davidofjoy@gmail.com>
2022-02-01 21:46:07 -0500

    Cmake update

---
+++ include/project_config.h
@@ -0,0 +1,3 @@
+// the configured options and settings for Tutorial
+constexpr int VRT_VERSION_MAJOR { 1 };
+constexpr int VRT_VERSION_MINOR {  };
---
+++ src/vulkan_ray_tracing.cpp
@@ -0,0 +1,11 @@
+﻿#include "vulkan_ray_tracing.h"
+#include "project_config.h"
+
+#include <iostream>
+
+int main()
+{
+	std::cout << "Vulkan Ray Tracing Version " << VRT_VERSION_MAJOR << "." << VRT_VERSION_MINOR << '\n';
+
+	return 0;
+}
