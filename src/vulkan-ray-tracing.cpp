db9d6cf093e2801300208fb5815b0e61cd8f8b10
David DiGioia <davidofjoy@gmail.com>
2022-02-01 20:24:14 -0500

    Initial commit

---
+++ src/vulkan-ray-tracing.cpp
@@ -0,0 +1,15 @@
+﻿#include "vulkan-ray-tracing.h"
+#include "project-config.h"
+
+#include <iostream>
+
+int main(int argc, char** argv)
+{
+	if (argc < 2) {
+		// report version
+		std::cout << "Vulkan Ray Tracing Version " << VRT_VERSION_MAJOR << "." << VRT_VERSION_MINOR << '\n';
+		return 1;
+	}
+
+	return 0;
+}
